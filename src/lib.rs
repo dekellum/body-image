62f52702b7dde3b4583c60c35ee2cea2fba0fd51
David Kellum <dek-oss@gravitext.com>
2018-01-30 14:51:40 -0800

    Introduce BarcWriter struct

--- src/lib.rs
+++ src/lib.rs
@@ -16,56 +16,62 @@ use hyper::client::{FutureResponse, Response};
 use tokio_core::reactor::Core;
 use tempfile::tempfile;
 
-fn resp_future(res: Response)
-    -> Box<Future<Item=(), Error=hyper::Error> + Send>
-{
-    println!("Response: {}", res.status());
-    println!("Headers:\n{}", res.headers());
+struct BarcWriter {}
 
-    match tempfile() {
-        Ok(mut tfile) => {
-            let mut length_read: usize = 0;
-            Box::new(res.body().for_each(move |chunk| {
-                length_read += chunk.len();
-                if length_read > 5_000 {
-                    Err(IoError::new(
-                        ErrorKind::Other,
-                        format!("too long: {}+", length_read)
-                    ).into())
-                } else {
-                    println!("chunk ({})", length_read);
-                    tfile.write_all(&chunk).map_err(From::from)
-                }
-            }))
+impl BarcWriter {
+    fn resp_future(&mut self, res: Response)
+                   -> Box<Future<Item=(), Error=hyper::Error> + Send>
+    {
+        println!("Response: {}", res.status());
+        println!("Headers:\n{}", res.headers());
+
+        match tempfile() {
+            Ok(mut tfile) => {
+                let mut length_read: usize = 0;
+                Box::new(res.body().for_each(move |chunk| {
+                    length_read += chunk.len();
+                    if length_read > 5_000 {
+                        Err(IoError::new(
+                            ErrorKind::Other,
+                            format!("too long: {}+", length_read)
+                        ).into())
+                    } else {
+                        println!("chunk ({})", length_read);
+                        tfile.write_all(&chunk).map_err(From::from)
+                    }
+                }))
+            }
+            Err(e) => Box::new(futerr::<(), _>(e.into()))
         }
-        Err(e) => Box::new(futerr::<(), _>(e.into()))
     }
-}
 
-fn example() -> Result<(), Error> {
-    let mut core = Core::new()?;
-    let client = Client::new(&core.handle());
+    fn example(&mut self) -> Result<(), Error> {
+        let mut core = Core::new()?;
+        let client = Client::new(&core.handle());
 
-    // hyper::uri::Uri, via std String parse and FromStr
-    let uri = "http://gravitext.com".parse()?;
+        // hyper::uri::Uri, via std String parse and FromStr
+        let uri = "http://gravitext.com".parse()?;
 
-    let fr: FutureResponse = client.get(uri); // FutureResponse
+        let fr: FutureResponse = client.get(uri); // FutureResponse
 
-    // FnOnce(Response) -> IntoFuture<Error=hyper::Error>
-    let work = fr.and_then(resp_future);
+        // FnOnce(Response) -> IntoFuture<Error=hyper::Error>
+        let work = fr.and_then(|res| self.resp_future(res));
 
-    core.run(work)?;
+        core.run(work)?;
 
-    Ok(())
+        Ok(())
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::example;
+    use super::*;
 
     #[test]
     fn text_example() {
-        match example() {
+        let mut bw = BarcWriter {};
+
+        match bw.example() {
             Ok(_) => println!("ok"),
             Err(e) => panic!("Error from work: {:?}", e)
         }
