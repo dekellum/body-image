4db6796c04bfbe98b49563467a422f100e67569e
David Kellum <dek-oss@gravitext.com>
2018-01-31 09:11:57 -0800

    map_err to failure's error

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,14 @@
 #![allow(dead_code)]
 
-extern crate failure; // #[macro_use]
+#[macro_use] extern crate failure;
 extern crate futures;
 extern crate hyper;
 extern crate tempfile;
 extern crate tokio_core;
 
-use failure::Error;
+use failure::Error as FlError;
 
-use std::io::{Error as IoError, ErrorKind, Write};
+use std::io::Write;
 use futures::{Future, Stream};
 use futures::future::err as futerr;
 use hyper::Client;
@@ -20,7 +20,7 @@ struct BarcWriter {}
 
 impl BarcWriter {
     fn resp_future(&mut self, res: Response)
-        -> Box<Future<Item=(), Error=hyper::Error> + Send>
+        -> Box<Future<Item=(), Error=FlError> + Send>
     {
         println!("Response: {}", res.status());
         println!("Headers:\n{}", res.headers());
@@ -28,24 +28,23 @@ impl BarcWriter {
         match tempfile() {
             Ok(mut tfile) => {
                 let mut length_read: usize = 0;
-                Box::new(res.body().for_each(move |chunk| {
-                    length_read += chunk.len();
-                    if length_read > 5_000 {
-                        Err(IoError::new(
-                            ErrorKind::Other,
-                            format!("too long: {}+", length_read)
-                        ).into())
-                    } else {
-                        println!("chunk ({})", length_read);
-                        tfile.write_all(&chunk).map_err(From::from)
-                    }
-                }))
+                let s = res.body().map_err(FlError::from).
+                    for_each(move |chunk| {
+                        length_read += chunk.len();
+                        if length_read > 50_000 {
+                            bail!("Response stream too long: {}+", length_read);
+                        } else {
+                            println!("chunk ({})", length_read);
+                            tfile.write_all(&chunk).map_err(FlError::from)
+                        }
+                    });
+                Box::new(s)
             }
             Err(e) => Box::new(futerr::<(), _>(e.into()))
         }
     }
 
-    fn example(&mut self) -> Result<(), Error> {
+    fn example(&mut self) -> Result<(), FlError> {
         let mut core = Core::new()?;
         let client = Client::new(&core.handle());
 
@@ -55,7 +54,7 @@ impl BarcWriter {
         let fr: FutureResponse = client.get(uri); // FutureResponse
 
         // FnOnce(Response) -> IntoFuture<Error=hyper::Error>
-        let work = fr.and_then(|res| self.resp_future(res));
+        let work = fr.map_err(FlError::from).and_then(|res| self.resp_future(res));
 
         core.run(work)?;
 
