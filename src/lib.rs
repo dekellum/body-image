681eb671605fc9bf1c8f76bffdac45d931d43a1f
David Kellum <dek-oss@gravitext.com>
2018-02-20 13:43:54 -0800

    Use hyper's Content-Length parsing

--- src/lib.rs
+++ src/lib.rs
@@ -19,11 +19,13 @@ use std::fmt;
 use std::fs::File;
 use std::io;
 use std::io::{Cursor, Read, Seek, SeekFrom, Write};
+use std::ops::Deref;
 use futures::{Future, Stream};
 use futures::future::err as futerr;
 use futures::future::result as futres;
 use hyper::{Chunk, Client};
 use hyper::client::compat::CompatFutureResponse;
+use hyper::header::{ContentLength, Header, Raw};
 use memmap::Mmap;
 use tempfile::tempfile;
 use tokio_core::reactor::Core;
@@ -450,8 +452,8 @@ fn resp_future(prolog: Prolog, tune: Tunables)
 fn check_length(v: &http::header::HeaderValue, max: u64)
     -> Result<u64, FlError>
 {
-    let v = v.to_str()?;
-    let l: u64 = v.parse()?;
+    let l = ContentLength::parse_header(&Raw::from(v.as_bytes()))?;
+    let l = *l.deref();
     if l > max {
         bail!("Response Content-Length too long: {}", l);
     }
