2f6cdda8fad63231bebad87d54cad163f947dd46
David Kellum <dek-oss@gravitext.com>
2018-04-07 10:09:47 -0700

    Add barc cp command

--- src/main.rs
+++ src/main.rs
@@ -11,15 +11,14 @@ use std::io;
 use std::process;
 use body_image::{Tunables, RequestRecorded, Recorded};
 use body_image::barc::{BarcFile, write_body, write_headers};
-use clap::{Arg, App, AppSettings, SubCommand};
+use clap::{Arg, ArgMatches, App, AppSettings, SubCommand};
 use failure::Error as Flare;
 
-#[cfg(feature = "client")]
 use body_image::barc::{CompressStrategy,
                        NoCompressStrategy,
                        GzipCompressStrategy};
 
-#[cfg(all(feature = "client", feature = "brotli"))]
+#[cfg(feature = "brotli")]
 use body_image::barc::BrotliCompressStrategy;
 
 fn main() {
@@ -39,63 +38,27 @@ fn run() -> Result<(), Flare> {
 
     match scname {
         "cat" => {
-            let mut parts = Parts::default();
-            if  subm.is_present("meta") ||
-                subm.is_present("req_headers") || subm.is_present("req_body") ||
-                subm.is_present("res_headers") || subm.is_present("res_body")
-            {
-                parts = Parts::falsie();
-                if subm.is_present("meta")        { parts.meta        = true; }
-                if subm.is_present("req_headers") { parts.req_headers = true; }
-                if subm.is_present("req_body")    { parts.req_body    = true; }
-                if subm.is_present("res_headers") { parts.res_headers = true; }
-                if subm.is_present("res_body")    { parts.res_body    = true; }
-            }
+            let parts = part_flags(subm);
             let files = subm.values_of("file").unwrap();
-            let mut start = StartPos::default();
-            let mut count = usize::max_value();
-            if let Some(v) = subm.value_of("index") {
-                if files.len() > 1 {
-                    bail!("--index flag can't be applied to more than one \
-                           input file");
-                }
-                let v = v.parse()?;
-                start = StartPos::Index(v);
-            }
-            if let Some(v) = subm.value_of("offset") {
-                if files.len() > 1 {
-                    bail!("--offset flag can't be applied to more than one \
-                           input file");
-                }
-                let v = v.parse()?;
-                start = StartPos::Offset(v);
-            }
-            if let Some(v) = subm.value_of("count") {
-                if files.len() > 1 {
-                    bail!("--count flag can't be applied to more than one \
-                           input file");
-                }
-                count = v.parse()?;
-            }
+            let (start, count) = filter_flags(subm)?;
             for f in files {
                 cat(f, &start, count, &parts)?;
             }
             Ok(())
         }
+        "cp" => {
+            let cs = compress_flags(subm)?;
+            let mut files = subm.values_of("file").unwrap();
+            let fout = subm.value_of("out-file").unwrap();
+            let (start, count) = filter_flags(subm)?;
+            for fin in files {
+                cp(fin, fout, &start, count, cs.as_ref())?;
+            }
+            Ok(())
+        }
         #[cfg(feature = "client")]
         "record" => {
-            let mut cs: Box<CompressStrategy> =
-                Box::new(NoCompressStrategy::default());
-            match subm.is_present("brotli") {
-                #[cfg(feature = "brotli")]
-                true => {
-                    cs = Box::new(BrotliCompressStrategy::default());
-                }
-                _ => {}
-            }
-            if subm.is_present("gzip") {
-                cs = Box::new(GzipCompressStrategy::default());
-            }
+            let cs = compress_flags(subm)?;
             record::record(subm.value_of("url").unwrap(),
                            subm.value_of("file").unwrap(),
                            cs.as_ref())
@@ -109,6 +72,71 @@ fn run() -> Result<(), Flare> {
             bail!("Sub-command \"{}\" not supported", scname);
         }
     }
+}
+
+// Parse the field selection flags into `Parts`
+fn part_flags(matches: &ArgMatches) -> Parts {
+    let mut parts = Parts::default();
+    if  matches.is_present("meta") ||
+        matches.is_present("req_headers") || matches.is_present("req_body") ||
+        matches.is_present("res_headers") || matches.is_present("res_body")
+    {
+        parts = Parts::falsie();
+        if matches.is_present("meta")        { parts.meta        = true; }
+        if matches.is_present("req_headers") { parts.req_headers = true; }
+        if matches.is_present("req_body")    { parts.req_body    = true; }
+        if matches.is_present("res_headers") { parts.res_headers = true; }
+        if matches.is_present("res_body")    { parts.res_body    = true; }
+    }
+    parts
+}
+
+// Parse the compress flags into a CompressStrategy
+fn compress_flags(matches: &ArgMatches) -> Result<Box<CompressStrategy>, Flare>
+{
+    let mut cs: Box<CompressStrategy> = Box::new(NoCompressStrategy::default());
+    if matches.is_present("brotli") {
+        if cfg!(feature = "brotli") {
+            cs = Box::new(BrotliCompressStrategy::default());
+        } else {
+            bail!("Brotli compression requires the \"broli\" build feature");
+        }
+    }
+    if matches.is_present("gzip") {
+        cs = Box::new(GzipCompressStrategy::default());
+    }
+    Ok(cs)
+}
+
+// Parse filter flags into a (StartPos, count) tupple, or error.
+fn filter_flags(matches: &ArgMatches) -> Result<(StartPos, usize), Flare> {
+    let files_len = matches.values_of("file").unwrap().len();
+    let mut start = StartPos::default();
+    let mut count = usize::max_value();
+    if let Some(v) = matches.value_of("index") {
+        if files_len > 1 {
+            bail!("--index flag can't be applied to more than one \
+                   input file");
+        }
+        let v = v.parse()?;
+        start = StartPos::Index(v);
+    }
+    if let Some(v) = matches.value_of("offset") {
+        if files_len > 1 {
+            bail!("--offset flag can't be applied to more than one \
+                   input file");
+        }
+        let v = v.parse()?;
+        start = StartPos::Offset(v);
+    }
+    if let Some(v) = matches.value_of("count") {
+        if files_len > 1 {
+            bail!("--count flag can't be applied to more than one \
+                   input file");
+        }
+        count = v.parse()?;
+    }
+    Ok((start, count))
 }
 
 enum StartPos {
@@ -170,6 +198,38 @@ fn cat(barc_path: &str, start: &StartPos, count: usize, parts: &Parts)
     Ok(())
 }
 
+fn cp(barc_in: &str,
+      barc_out: &str,
+      start: &StartPos,
+      count: usize,
+      strategy: &CompressStrategy)
+    -> Result<(), Flare>
+{
+    if barc_in == barc_out {
+        bail!("BARC input and output must be different files");
+    }
+    let bfin = BarcFile::new(barc_in);
+    let tune = Tunables::new();
+    let mut reader = bfin.reader()?;
+    if let StartPos::Offset(o) = *start {
+        reader.seek(o)?;
+    }
+    let bfout = BarcFile::new(barc_out);
+    let mut writer = bfout.writer()?;
+
+    let mut i = 0;
+    while let Some(record) = reader.read(&tune)? {
+        if let StartPos::Index(s) = *start {
+            if i < s { i += 1; continue; }
+        }
+        i += 1;
+        if i > count { break; }
+        writer.write(&record, strategy)?;
+    }
+
+    Ok(())
+}
+
 fn setup_cli() -> App<'static, 'static> {
     let appl = App::new("barc")
         .version(crate_version!())
@@ -210,7 +270,7 @@ fn setup_cli() -> App<'static, 'static> {
             Arg::with_name("file")
                 .required(true)
                 .index(2)
-                .value_name("BARC-FILE")
+                .value_name("BARC-OUT-FILE")
                 .help("path to BARC file to create/append"),
         ]);
 
@@ -262,8 +322,55 @@ fn setup_cli() -> App<'static, 'static> {
                 .value_name("BARC-FILE")
                 .help("BARC file paths from which to read")
         ]);
-    appl.subcommand(rec)
-        .subcommand(cat)
+
+    let cp = SubCommand::with_name("cp")
+        .setting(AppSettings::DeriveDisplayOrder)
+        .setting(AppSettings::DontDelimitTrailingValues)
+        .about("Copy BARC records from input(s) to an output file")
+        .after_help(
+            "By default copies all records from input file(s) to the output \
+             file. The flags (--offset, --index, --count) can be used to \
+             filter a single input file.")
+        .args(&[
+            Arg::with_name("offset")
+                .short("o")
+                .long("offset")
+                .number_of_values(1)
+                .help("Offset in bytes to first input record"),
+            Arg::with_name("index")
+                .short("i")
+                .long("index")
+                .number_of_values(1)
+                .conflicts_with("start-offset")
+                .help("Zero based index to first input record (Default: 0)"),
+            Arg::with_name("count")
+                .short("c")
+                .long("count")
+                .number_of_values(1)
+                .help("Count of records to print (Default: all until EOF)"),
+            Arg::with_name("gzip")
+                .short("z")
+                .long("gzip")
+                .help("Use gzip compression strategy"),
+            Arg::with_name("brotli")
+                .short("b")
+                .long("brotli")
+                .conflicts_with("gzip")
+                .help("Use Brotli compression strategy (brotli feature)"),
+            Arg::with_name("file")
+                .required(true)
+                .multiple(true)
+                .value_name("BARC-IN-FILE")
+                .help("BARC file paths from which to read and final to write"),
+            Arg::with_name("out-file")
+                .required(true)
+                .value_name("BARC-OUT-FILE")
+                .help("BARC file path to create/append")
+        ]);
+
+    appl.subcommand(cat)
+        .subcommand(cp)
+        .subcommand(rec)
 }
 
 fn setup_logger(debug_flags: u64) -> Result<(), Flare> {
