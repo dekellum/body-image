e9b3f9995fa37b9c28a2d3c18e02da9398ec2655
David Kellum <dek-oss@gravitext.com>
2018-04-30 15:44:30 -0700

    ReadPos/Slice refinements

--- src/read_pos.rs
+++ src/read_pos.rs
@@ -24,22 +24,20 @@ pub trait PosRead {
 }
 
 /// Re-implements `Read` and `Seek` over a shared `File` reference using
-/// _only_ positioned reads via `PosRead`, and by maintaining an instance
-/// independent position.
+/// _only_ positioned reads, and by maintaining an instance independent
+/// position.
 ///
 /// `ReadPos` supports independent instances on the same `File` (and handle)
-/// without needing a path to open an independent `File`.  Thus it is
-/// compatible with "unnamed" (not linked) temporary files. Note that unix
-/// `dup`/`dup2` and `File::try_clone` do not provide independent file
-/// positions.
+/// without needing a path to open an independent new `File` instance.  Thus
+/// it is compatible with "unnamed" (not linked) temporary files, and can
+/// reduce the total number of utilized file handles.  Note that unix
+/// `dup`/`dup2` and the standard `File::try_clone` do _not_ provide
+/// independent file positions.
 ///
-/// ### Current Limitations
-///
-/// A (file) length is arbitrarily passed in and used solely for handling
-/// SeekFrom::End. Seeking past end of file is allowed by the platforms and
-/// `File` in any case.  This size is not checked or updated via `File`
-/// metadata, which could result in surprising behavior if concurrent updates
-/// were possible.
+/// A length is arbitrarily passed in and used solely for handling
+/// `SeekFrom::End`. Reads are not constrained by this length. The length is
+/// neither checked nor updated via file metadata, and could deviate from the
+/// underlying file length if concurrent write or truncation is possible.
 #[derive(Debug)]
 pub struct ReadPos {
     pos: u64,
@@ -47,6 +45,24 @@ pub struct ReadPos {
     file: Arc<File>,
 }
 
+/// Re-implements `Read` and `Seek` over a shared `File` reference using
+/// _only_ positioned reads and by maintaining instance independent start,
+/// end, and position index values. Seeks are relative, so a seek to
+/// `SeekFrom::Start(0)` is always the first byte of the slice, including when
+/// the absolute start index is greater-than 0.
+///
+/// `ReadSlice` supports independent instances on the same `File` (and handle)
+/// without needing a path to open an independent new `File` instance.  Thus
+/// it is compatible with "unnamed" (not linked) temporary files, and can
+/// reduce the total number of utilized file handles.  Note that unix
+/// `dup`/`dup2` and the standard `File::try_clone` do _not_ provide
+/// independent file positions.
+///
+/// Fixed start and end offsets are passed in and used to constrain reads and
+/// interpret `SeekFrom::End`. Seeking past end of file is allowed by the
+/// platforms and `File` in any case.  These offsets are neither checked nor
+/// updated via file metadata, and the end offset could deviate from the
+/// underlying file length if concurrent writes or truncation is possible.
 #[derive(Debug)]
 pub struct ReadSlice {
     start: u64,
@@ -73,6 +89,23 @@ impl ReadPos {
     /// New instance by `File` reference and fixed file length.
     pub(crate) fn new(file: Arc<File>, length: u64) -> ReadPos {
         ReadPos { pos: 0, length, file }
+    }
+
+    /// Return a new and independent `ReadSlice` for the same file, for the
+    /// range of byte offsets `start..end`.
+    pub(crate) fn as_read_slice(&self, start: u64, end: u64) -> ReadSlice {
+        ReadSlice::new(self.file.clone(), start, end)
+    }
+
+    /// Return the length as provided on construction. This may differ from
+    /// the underlying file length.
+    pub fn len(&self) -> u64 {
+        self.length
+    }
+
+    /// Return `true` if length is 0.
+    pub fn is_empty(&self) -> bool {
+        self.length == 0
     }
 
     /// Seek by signed offset from an origin, checking for underflow and
@@ -103,8 +136,8 @@ impl ReadPos {
 
 impl Clone for ReadPos {
     /// Return a new, independent `ReadPos` with the same length and file
-    /// reference as self, and with position 0 (ignore self's current
-    /// position).
+    /// reference as self, and with position 0 (ignores the current
+    /// position of self).
     fn clone(&self) -> ReadPos {
         ReadPos { pos: 0, length: self.length, file: self.file.clone() }
     }
@@ -151,6 +184,42 @@ impl ReadSlice {
         ReadSlice { start, pos: start, end, file }
     }
 
+    pub(crate) fn start(&self) -> u64 {
+        self.start
+    }
+
+    /// Return the total size of the slice in bytes. This is based on the
+    /// start and end offsets as constructed and can differ from the
+    /// underlying file length.
+    pub fn len(&self) -> u64 {
+        self.end - self.start
+    }
+
+    /// Return `true` if length is 0.
+    pub fn is_empty(&self) -> bool {
+        self.len() == 0
+    }
+
+    pub(crate) fn file_ref(&self) -> &File {
+        &self.file
+    }
+
+    /// Like `PosRead::pread`, but using an absolute (internal) position
+    /// instead of the external, relative offset.
+    fn pread_abs(&self, buf: &mut [u8], abspos: u64) -> io::Result<usize> {
+        if abspos < self.end {
+            let mlen = self.end - abspos; // positive/no-underflow per above
+            if (buf.len() as u64) <= mlen {
+                self.file.pread(buf, abspos)
+            } else {
+                // safe cast: mlen < buf.len which is already usize
+                self.file.pread(&mut buf[..(mlen as usize)], abspos)
+            }
+        } else {
+            Ok(0)
+        }
+    }
+
     /// Seek by signed offset from an (absolute) origin, checking for
     /// underflow and overflow.
     fn seek_from(&mut self, origin: u64, offset: i64) -> io::Result<u64> {
@@ -175,8 +244,10 @@ impl ReadSlice {
         }
     }
 
-    /// Seek to abspos and return relative position within slice. Return Error
-    /// if abspos is before start. Positions beyond end are allowed.
+    /// Seek by absolute position, validated with the start index. Return the
+    /// new relative position, or Error if the absolute position is before
+    /// start. Like with a regular File, positions beyond end are allowed, and
+    /// this is checked on reads.
     fn seek_to(&mut self, abspos: u64) -> io::Result<u64> {
         if abspos < self.start {
             Err(Error::new(
@@ -190,18 +261,23 @@ impl ReadSlice {
     }
 }
 
+impl Clone for ReadSlice {
+    /// Return a new, independent `ReadSlice` with the same start, end and
+    /// file reference as self, and positioned at start (ignores the current
+    /// position of self).
+    fn clone(&self) -> ReadSlice {
+        ReadSlice { start: self.start,
+                    pos:   self.start,
+                    end:   self.end,
+                    file:  self.file.clone() }
+    }
+}
+
 impl PosRead for ReadSlice {
-    #[inline]
     fn pread(&self, buf: &mut [u8], offset: u64) -> io::Result<usize> {
         let pos = self.start.saturating_add(offset);
         if pos < self.end {
-            let mlen = self.end - pos; // positive/no-underflow per above
-            if (buf.len() as u64) <= mlen {
-                self.file.pread(buf, pos)
-            } else {
-                // safe cast: mlen < buf.len which is already usize
-                self.file.pread(&mut buf[..(mlen as usize)], pos)
-            }
+            self.pread_abs(buf, pos)
         } else {
             Ok(0)
         }
@@ -210,7 +286,7 @@ impl PosRead for ReadSlice {
 
 impl Read for ReadSlice {
     fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
-        let len = self.pread(buf, self.pos)?;
+        let len = self.pread_abs(buf, self.pos)?;
         self.pos += len as u64;
         Ok(len)
     }
