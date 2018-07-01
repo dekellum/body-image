17fd555e99f6a1c07f575cd2fbae083d9a0f3826
David Kellum <dek-oss@gravitext.com>
2018-07-01 14:31:35 -0700

    client tests with request body, via stub

--- src/client.rs
+++ src/client.rs
@@ -941,6 +941,74 @@ mod tests {
         }
     }
 
+    #[test]
+    fn test_image_post_via_stub() {
+        assert!(*LOG_SETUP);
+        let tune = Tuner::new()
+            .set_buffer_size_fs(17)
+            .finish();
+        let mut body = BodySink::with_fs(tune.temp_dir()).unwrap();
+        body.write_all(
+           "Lorem ipsum dolor sit amet, consectetur adipiscing elit, \
+            sed do eiusmod tempor incididunt ut labore et dolore magna \
+            aliqua. Ut enim ad minim veniam, quis nostrud exercitation \
+            ullamco laboris nisi ut aliquip ex ea commodo \
+            consequat. Duis aute irure dolor in reprehenderit in \
+            voluptate velit esse cillum dolore eu fugiat nulla \
+            pariatur. Excepteur sint occaecat cupidatat non proident, \
+            sunt in culpa qui officia deserunt mollit anim id est \
+            laborum."
+        ).unwrap();
+        let body = body.prepare().unwrap();
+
+        let rq: RequestRecord<hyper::Body> = http::Request::builder()
+            .method(http::Method::POST)
+            .header(http::header::USER_AGENT, &user_agent()[..])
+            // To avoid 27 chunks
+            // .header(http::header::CONTENT_LENGTH, "445")
+            .uri("http://foobar.com")
+            .record_body_image(body, &tune)
+            .unwrap();
+        let client = hyper_stub::proxy_client_fn_ok(|req| {
+            hyper::Response::new(req.into_body())
+        });
+        let mut rt = tokio::runtime::Runtime::new().unwrap();
+        let res = rt.block_on(request_dialog(&client, rq, &tune));
+        match res {
+            Ok(dl) => {
+                println!("{:#?}", dl);
+                assert_eq!(dl.res_body.len(), 445);
+            }
+            Err(e) => {
+                panic!("failed with: {}", e);
+            }
+        }
+    }
+
+    #[test]
+    fn test_byte_post_via_stub() {
+        assert!(*LOG_SETUP);
+        let tune = Tunables::new();
+        let rq: RequestRecord<hyper::Body> = http::Request::builder()
+            .method(http::Method::POST)
+            .uri("http://foobar.com")
+            .record_body(&b"stub"[..])
+            .unwrap();
+        let client = hyper_stub::proxy_client_fn_ok(|req| {
+            hyper::Response::new(req.into_body())
+        });
+        let mut rt = tokio::runtime::Runtime::new().unwrap();
+        let res = rt.block_on(request_dialog(&client, rq, &tune));
+        match res {
+            Ok(dl) => {
+                assert_eq!(dl.res_body.len(), 4);
+            }
+            Err(e) => {
+                panic!("failed with: {}", e);
+            }
+        }
+    }
+
     // Use lazy static to ensure we only setup logging once (by first test and
     // thread)
     lazy_static! {
