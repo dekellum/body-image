d30cfb5bc572b13bc486db8f710a60f5e3c9549e
David Kellum <dek-oss@gravitext.com>
2018-07-18 14:44:24 -0700

    Remove replaced parallel and large live tests

--- src/async/tests/live.rs
+++ src/async/tests/live.rs
@@ -1,6 +1,4 @@
-use ::std::time::Duration;
 use ::logger::LOG_SETUP;
-use ::Tuner;
 
 use async::*;
 
@@ -58,63 +56,4 @@ fn test_not_found() {
     assert!(dl.res_body.is_ram());
     assert!(dl.res_body.len() > 0);
     assert!(dl.res_body.len() < 1000);
-}
-
-#[test]
-fn test_large_http() {
-    assert!(*LOG_SETUP);
-    let tune = Tuner::new()
-        .set_max_body_ram(64 * 1024)
-        .finish();
-    let req = create_request(
-        "http://gravitext.com/images/jakarta_slum.jpg"
-    ).unwrap();
-    let dl = fetch(req, &tune).unwrap();
-    println!("Response {:#?}", dl);
-
-    assert!(dl.res_body.len() > (64 * 1024));
-    assert!(!dl.res_body.is_ram());
-}
-
-#[test]
-fn test_large_parallel_constrained() {
-    assert!(*LOG_SETUP);
-    let tune = Tuner::new()
-        .set_max_body_ram(64 * 1024)
-        .set_res_timeout(Duration::from_secs(15))
-        .set_body_timeout(Duration::from_secs(55))
-        .finish();
-
-    let mut pool = tokio::executor::thread_pool::Builder::new();
-    pool.name_prefix("tpool-")
-        .pool_size(1)
-        .max_blocking(2);
-    let mut rt = tokio::runtime::Builder::new()
-        .threadpool_builder(pool)
-        .build().unwrap();
-
-    let client = hyper::Client::new();
-
-    let rq0 = create_request(
-        "http://cache.ruby-lang.org/pub/ruby/1.8/ChangeLog-1.8.2"
-    ).unwrap();
-    let rq1 = create_request(
-        "http://cache.ruby-lang.org/pub/ruby/1.8/ChangeLog-1.8.3"
-    ).unwrap();
-
-    let res = rt.block_on(
-        request_dialog(&client, rq0, &tune)
-            .join(request_dialog(&client, rq1, &tune))
-    );
-    match res {
-        Ok((dl0, dl1)) => {
-            assert_eq!(dl0.res_body.len(), 333_210);
-            assert_eq!(dl1.res_body.len(), 134_827);
-            assert!(!dl0.res_body.is_ram());
-            assert!(!dl1.res_body.is_ram());
-        }
-        Err(e) => {
-            panic!("failed with: {}", e);
-        }
-    }
 }
