652f8a2f188d958373aa3614d2972b0b34b3ddf9
David Kellum <dek-oss@gravitext.com>
2018-07-19 11:08:18 -0700

    Add stub test_echo_server

--- src/async/tests/stub.rs
+++ src/async/tests/stub.rs
@@ -5,6 +5,9 @@ use ::Tuner;
 
 use async::*;
 
+use async::hyper::client::connect::Connect;
+use async::hyper::Client;
+
 fn get_request(url: &str)
     -> Result<RequestRecord<hyper::Body>, Flare>
 {
@@ -46,14 +49,7 @@ fn test_fs_image_post() {
     body.write_all(vec![b'a'; 445]).unwrap();
     let body = body.prepare().unwrap();
 
-    let rq: RequestRecord<hyper::Body> = http::Request::builder()
-        .method(http::Method::POST)
-        .header(http::header::USER_AGENT, &user_agent()[..])
-        // To avoid 27 chunks
-        // .header(http::header::CONTENT_LENGTH, "445")
-        .uri("http://foobar.com")
-        .record_body_image(body, &tune)
-        .unwrap();
+    let rq: RequestRecord<hyper::Body> = post_request(body, &tune);
     let client = hyper_stub::proxy_client_fn_ok(|req| {
         hyper::Response::new(req.into_body())
     });
@@ -141,4 +137,73 @@ fn test_large_concurrent_constrained() {
             panic!("failed with: {}", e);
         }
     }
+}
+
+#[test]
+fn test_echo_server() {
+    assert!(*LOG_SETUP);
+    let tune = Tunables::default();
+    let body = fs_body_image(194_767);
+    let rq = post_request(body, &tune);
+    let client = echo_server_stub();
+    let mut rt = tokio::runtime::Runtime::new().unwrap();
+    let res = rt.block_on(request_dialog(&client, rq, &tune));
+    match res {
+        Ok(dl) => {
+            assert_eq!(dl.res_body.len(), 194_767);
+        }
+        Err(e) => {
+            panic!("failed with: {}", e);
+        }
+    }
+}
+
+fn fs_body_image(size: usize) -> BodyImage {
+    let tune = Tunables::default();
+    let mut body = BodySink::with_fs(tune.temp_dir()).unwrap();
+    body.write_all(vec![1; size]).unwrap();
+    body.prepare().unwrap()
+}
+
+fn post_request<T>(body: BodyImage, tune: &Tunables) -> RequestRecord<T>
+    where T: hyper::body::Payload + Send,
+          http::request::Builder: RequestRecorder<T>
+{
+    http::Request::builder()
+        .method(http::Method::POST)
+        .uri("http://foobar.com")
+        .header(http::header::USER_AGENT, &user_agent()[..])
+        // To avoid chunked transfer
+        // .header(http::header::CONTENT_LENGTH, body.len())
+        .record_body_image(body, &tune)
+        .unwrap()
+}
+
+/// A Client stub to a body echo'ing server, which buffers complete requests
+/// (potentially to disk) using AsyncBodySink and responds with AsyncBodyImage
+fn echo_server_stub() -> Client<impl Connect> {
+    hyper_stub::proxy_client_fn(|req| {
+        let tune = Tuner::new()
+            .set_buffer_size_fs(2734)
+            .set_max_body_ram(15_000)
+            .finish();
+        let asink = AsyncBodySink::new(
+            BodySink::with_ram_buffers(4),
+            tune
+        );
+        req.into_body()
+            .from_err::<Flare>()
+            .forward(asink)
+            .and_then(move |(_strm, asink)| {
+                let tune = Tuner::new().set_buffer_size_fs(4972).finish();
+                let bi = asink.into_inner().prepare()?;
+                Ok(http::Response::builder()
+                   .status(200)
+                   .body(hyper::Body::wrap_stream(
+                       AsyncBodyImage::new(bi, &tune)
+                   ))?
+                )
+            })
+            .map_err(|e| e.compat())
+    })
 }
