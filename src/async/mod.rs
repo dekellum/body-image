edc7409fea6d7cc1a56b2baf66fccb575ec05203
David Kellum <dek-oss@gravitext.com>
2018-07-25 15:48:36 -0700

    live_test feature becomes more generic *may_fail* feature

--- src/async/mod.rs
+++ src/async/mod.rs
@@ -554,5 +554,7 @@ mod tests {
     #[cfg(feature = "mmap")]        mod futures;
                                     mod stub;
                                     mod server;
-    #[cfg(feature = "live_test")]   mod live;
+
+    /// These tests may fail because they depend on public web servers
+    #[cfg(feature = "may_fail")]    mod live;
 }
