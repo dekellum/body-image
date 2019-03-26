b9084fc5b8e3770adf8b9ac669ef1e88454d80c0
David Kellum <dek-oss@gravitext.com>
2019-03-26 15:28:42 -0700

    make MSRV build.rs check more maintainable

--- body-image/build.rs
+++ body-image/build.rs
@@ -1,15 +1,22 @@
-extern crate version_check;
+static PACKAGE: &str = "body-image";
+static MSRV: &str = "1.32.0";
 
 fn main() {
-    match version_check::is_min_version("1.32.0") {
+    match version_check::is_min_version(MSRV) {
         Some((true, actual_v)) => {
-            eprintln!("body-image MSRV test passed: {} (actual)", actual_v);
+            eprintln!(
+                "{} MSRV test passed: {} (actual)",
+                PACKAGE, actual_v);
         }
         Some((false, actual_v)) => {
-            panic!("body-image MSRV is 1.32.0 > {} (this rustc)", actual_v);
+            panic!(
+                "{} MSRV is {} > {} (this rustc)",
+                PACKAGE, MSRV, actual_v);
         }
         None => {
-            panic!("couldn't query version info from rustc");
+            panic!(
+                "{}: couldn't query version info from rustc",
+                PACKAGE);
         }
     }
 }
