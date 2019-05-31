8d6a24a995c881556619a46cf5849038b56dda48
David Kellum <dek-oss@gravitext.com>
2019-05-31 11:51:07 -0700

    incomplete impl Sink03 for UniBodySink, test without compat

--- body-image-futio/src/uni_sink.rs
+++ body-image-futio/src/uni_sink.rs
@@ -5,6 +5,12 @@ use tokio_threadpool;
 
 use body_image::{BodyError, BodySink, Tunables};
 
+#[cfg(feature = "futures03")] use {
+    std::pin::Pin,
+    std::task::{Context, Poll as Poll03},
+    futures03::sink::Sink as Sink03,
+};
+
 use crate::{FutioError, UniBodyBuf};
 
 /// Adaptor for `BodySink` implementing the `futures::Sink` trait.  This
@@ -101,5 +107,78 @@ impl Sink for UniBodySink {
 
     fn close(&mut self) -> Poll<(), FutioError> {
         Ok(Async::Ready(()))
+    }
+}
+
+#[cfg(feature = "futures03")]
+macro_rules! unblock_03 {
+    ($c:ident, || $b:block) => (match tokio_threadpool::blocking(|| $b) {
+        Ok(Async::Ready(Ok(_))) => (),
+        Ok(Async::Ready(Err(e))) => return Err(e.into()),
+        Ok(Async::NotReady) => {
+            // FIXME: Can't give the UniBodyBuf back, so no choice but panic!?
+            // See also FIXME below.
+            panic!("No blocking backup thread available");
+        }
+        Err(e) => return Err(FutioError::Other(Box::new(e)))
+    })
+}
+
+#[cfg(feature = "futures03")]
+impl Sink03<UniBodyBuf> for UniBodySink {
+    type SinkError = FutioError;
+
+    fn poll_ready(self: Pin<&mut Self>, _cx: &mut Context<'_>)
+        -> Poll03<Result<(), FutioError>>
+    {
+        // FIXME: Can't really decide this, because we don't know the length of
+        // the next item?!
+        //
+        // However, see current implementation (0.3.0-alpha.16) of
+        // `futures_util::compat::Compat01As03Sink`.  It looks like the only,
+        // rather complicated, way to deal with this fully is to only buffer
+        // one item in `start_send`, then actually do the potentially blocking
+        // parts in poll_ready, flush, close?
+        Poll03::Ready(Ok(()))
+    }
+
+    fn start_send(self: Pin<&mut Self>, buf: UniBodyBuf)
+        -> Result<(), FutioError>
+    {
+        let this = self.get_mut();
+
+        let new_len = this.body.len() + (buf.remaining() as u64);
+        if new_len > this.tune.max_body() {
+            return Err(BodyError::BodyTooLong(new_len).into());
+        }
+        if this.body.is_ram() && new_len > this.tune.max_body_ram() {
+            unblock_03!(buf, || {
+                debug!("to write back file (blocking, len: {})", new_len);
+                this.body.write_back(this.tune.temp_dir())
+            })
+        }
+        if this.body.is_ram() {
+            debug!("to save buf (len: {})", buf.remaining());
+            this.body.write_all(&buf).map_err(FutioError::from)?;
+        } else {
+            unblock_03!(buf, || {
+                debug!("to write buf (blocking, len: {})", buf.remaining());
+                this.body.write_all(&buf)
+            })
+        }
+
+        Ok(())
+    }
+
+    fn poll_flush(self: Pin<&mut Self>, _cx: &mut Context<'_>)
+        -> Poll03<Result<(), FutioError>>
+    {
+        Poll03::Ready(Ok(()))
+    }
+
+    fn poll_close(self: Pin<&mut Self>, _cx: &mut Context<'_>)
+        -> Poll03<Result<(), FutioError>>
+    {
+        Poll03::Ready(Ok(()))
     }
 }
