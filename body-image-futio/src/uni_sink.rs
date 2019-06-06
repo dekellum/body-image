e77bef7a413640941ec8887ba5c821c9111814e4
David Kellum <dek-oss@gravitext.com>
2019-06-06 15:38:42 -0700

    Initial impl of tardy-ish/buffering UniBodySink

--- body-image-futio/src/uni_sink.rs
+++ body-image-futio/src/uni_sink.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 use bytes::Buf;
 use futures::{Async, AsyncSink, Poll, Sink, StartSend};
 use tao_log::debug;
@@ -28,10 +30,21 @@ use crate::{FutioError, UniBodyBuf};
 /// will appear *full*, with `start_send` returning
 /// `Ok(AsyncSink::NotReady(chunk)`, until a backup thread becomes available
 /// or any timeout occurs.
-#[derive(Debug)]
 pub struct UniBodySink {
     body: BodySink,
     tune: Tunables,
+    #[cfg(feature = "futures_03")]
+    buf: Option<UniBodyBuf>
+}
+
+impl fmt::Debug for UniBodySink {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("UniBodySink")
+            .field("body", &self.body)
+            .field("tune", &self.tune)
+            //FIXME: buf?
+            .finish()
+    }
 }
 
 impl UniBodySink {
@@ -40,7 +53,11 @@ impl UniBodySink {
     /// *Note*: Both `BodyImage` and `Tunables` are `Clone` (inexpensive), so
     /// that can be done beforehand to preserve owned copies.
     pub fn new(body: BodySink, tune: Tunables) -> UniBodySink {
-        UniBodySink { body, tune }
+        UniBodySink {
+            body,
+            tune,
+            #[cfg(feature = "futures_03")] buf: None
+        }
     }
 
     /// The inner `BodySink` as constructed.
@@ -114,71 +131,83 @@ impl Sink for UniBodySink {
 macro_rules! unblock_03 {
     ($c:ident, || $b:block) => (match tokio_threadpool::blocking(|| $b) {
         Ok(Async::Ready(Ok(_))) => (),
-        Ok(Async::Ready(Err(e))) => return Err(e.into()),
+        Ok(Async::Ready(Err(e))) => return (Poll03::Ready(Err(e.into())), None),
         Ok(Async::NotReady) => {
-            // FIXME: Can't give the UniBodyBuf back, so no choice but panic!?
-            // See also FIXME below.
-            panic!("No blocking backup thread available");
+            debug!("No blocking backup thread available -> NotReady");
+            return (Poll03::Pending, Some($c));
         }
-        Err(e) => return Err(FutioError::Other(Box::new(e)))
+        Err(e) => return (Poll03::Ready(Err(FutioError::Other(Box::new(e)))), None)
     })
+}
+
+#[cfg(feature = "futures_03")]
+impl UniBodySink {
+    fn poll_send(&mut self, _cx: &mut Context<'_>, buf: UniBodyBuf)
+        -> (Poll03<Result<(), FutioError>>, Option<UniBodyBuf>)
+    {
+        let new_len = self.body.len() + (buf.remaining() as u64);
+        if new_len > self.tune.max_body() {
+            return (Poll03::Ready(Err(BodyError::BodyTooLong(new_len).into())), None);
+        }
+        if self.body.is_ram() && new_len > self.tune.max_body_ram() {
+            unblock_03!(buf, || {
+                debug!("to write back file (blocking, len: {})", new_len);
+                self.body.write_back(self.tune.temp_dir())
+            })
+        }
+        if self.body.is_ram() {
+            debug!("to save buf (len: {})", buf.remaining());
+            if let Err(e) = self.body.write_all(&buf).map_err(FutioError::from) {
+                return (Poll03::Ready(Err(e)), None);
+            }
+        } else {
+            unblock_03!(buf, || {
+                debug!("to write buf (blocking, len: {})", buf.remaining());
+                self.body.write_all(&buf)
+            })
+        }
+
+        (Poll03::Ready(Ok(())), None)
+    }
 }
 
 #[cfg(feature = "futures_03")]
 impl Sink03<UniBodyBuf> for UniBodySink {
     type SinkError = FutioError;
 
-    fn poll_ready(self: Pin<&mut Self>, _cx: &mut Context<'_>)
+    fn poll_ready(self: Pin<&mut Self>, cx: &mut Context<'_>)
         -> Poll03<Result<(), FutioError>>
     {
-        // FIXME: Can't really decide this, because we don't know the length of
-        // the next item?!
-        //
-        // However, see current implementation (0.3.0-alpha.16) of
-        // `futures_util::compat::Compat01As03Sink`.  It looks like the only,
-        // rather complicated, way to deal with this fully is to only buffer
-        // one item in `start_send`, then actually do the potentially blocking
-        // parts in poll_ready, flush, close?
-        Poll03::Ready(Ok(()))
+        self.poll_flush(cx)
     }
 
     fn start_send(self: Pin<&mut Self>, buf: UniBodyBuf)
         -> Result<(), FutioError>
     {
         let this = self.get_mut();
-
-        let new_len = this.body.len() + (buf.remaining() as u64);
-        if new_len > this.tune.max_body() {
-            return Err(BodyError::BodyTooLong(new_len).into());
-        }
-        if this.body.is_ram() && new_len > this.tune.max_body_ram() {
-            unblock_03!(buf, || {
-                debug!("to write back file (blocking, len: {})", new_len);
-                this.body.write_back(this.tune.temp_dir())
-            })
-        }
-        if this.body.is_ram() {
-            debug!("to save buf (len: {})", buf.remaining());
-            this.body.write_all(&buf).map_err(FutioError::from)?;
-        } else {
-            unblock_03!(buf, || {
-                debug!("to write buf (blocking, len: {})", buf.remaining());
-                this.body.write_all(&buf)
-            })
-        }
-
+        assert!(this.buf.is_none());
+        this.buf = Some(buf);
         Ok(())
     }
 
-    fn poll_flush(self: Pin<&mut Self>, _cx: &mut Context<'_>)
+    fn poll_flush(self: Pin<&mut Self>, cx: &mut Context<'_>)
         -> Poll03<Result<(), FutioError>>
     {
-        Poll03::Ready(Ok(()))
+        let this = self.get_mut();
+        if let Some(buf) = this.buf.take() {
+            let (poll, back) = this.poll_send(cx, buf);
+            if back.is_some() {
+                this.buf = back;
+            }
+            poll
+        } else {
+            Poll03::Ready(Ok(()))
+        }
     }
 
-    fn poll_close(self: Pin<&mut Self>, _cx: &mut Context<'_>)
+    fn poll_close(self: Pin<&mut Self>, cx: &mut Context<'_>)
         -> Poll03<Result<(), FutioError>>
     {
-        Poll03::Ready(Ok(()))
+        self.poll_flush(cx)
     }
 }
