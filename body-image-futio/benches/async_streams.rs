2f932b85e5ed55f3219c2b4fb2ab7955406b6de6
David Kellum <dek-oss@gravitext.com>
2019-08-30 12:57:52 -0700

    add benchmarks with dispatch, CtRuntime, etc.

--- body-image-futio/benches/async_streams.rs
+++ body-image-futio/benches/async_streams.rs
@@ -7,11 +7,14 @@ use std::cmp;
 use std::fs;
 use std::path::{Path, PathBuf};
 
+use blocking_permit::DispatchPool;
 use futures::future;
 use futures::stream::{Stream, StreamExt};
 use rand::seq::SliceRandom;
 use test::Bencher;
-use tokio;
+use tokio::runtime::{Runtime as ThRuntime, Builder as ThBuilder};
+use tokio::runtime::current_thread::Runtime as CtRuntime;
+use lazy_static::lazy_static;
 
 use body_image::{BodyError, BodySink, BodyImage, Tunables, Tuner};
 use body_image_futio::*;
@@ -24,7 +27,7 @@ fn stream_01_ram_pregather(b: &mut Bencher) {
     let sink = BodySink::with_ram_buffers(1024);
     let mut body = sink_data(sink).unwrap();
     body.gather();
-    let mut rt = tokio::runtime::Runtime::new().unwrap();
+    let mut rt = ThRuntime::new().unwrap();
     b.iter(|| {
         let stream = AsyncBodyImage::new(body.clone(), &tune);
         summarize_stream(stream, &mut rt);
@@ -38,7 +41,7 @@ fn stream_02_ram(b: &mut Bencher) {
     let tune = Tunables::default();
     let sink = BodySink::with_ram_buffers(1024);
     let body = sink_data(sink).unwrap();
-    let mut rt = tokio::runtime::Runtime::new().unwrap();
+    let mut rt = ThRuntime::new().unwrap();
     b.iter(|| {
         let stream = AsyncBodyImage::new(body.clone(), &tune);
         summarize_stream(stream, &mut rt);
@@ -52,7 +55,7 @@ fn stream_03_ram_uni(b: &mut Bencher) {
     let tune = Tunables::default();
     let sink = BodySink::with_ram_buffers(1024);
     let body = sink_data(sink).unwrap();
-    let mut rt = tokio::runtime::Runtime::new().unwrap();
+    let mut rt = ThRuntime::new().unwrap();
     b.iter(|| {
         let stream = UniBodyImage::new(body.clone(), &tune);
         summarize_stream(stream, &mut rt);
@@ -67,7 +70,7 @@ fn stream_04_ram_gather(b: &mut Bencher) {
     let tune = Tunables::default();
     let sink = BodySink::with_ram_buffers(1024);
     let body = sink_data(sink).unwrap();
-    let mut rt = tokio::runtime::Runtime::new().unwrap();
+    let mut rt = ThRuntime::new().unwrap();
     b.iter(|| {
         let mut body = body.clone();
         body.gather();
@@ -77,13 +80,13 @@ fn stream_04_ram_gather(b: &mut Bencher) {
     rt.shutdown_on_idle();
 }
 
-// `AsyncBodyImage` in `FsRead`, default buffer size (64K)
+// `AsyncBodyImage` in `FsRead`, default buffer size (64K), threaded runtime
 #[bench]
 fn stream_10_fsread(b: &mut Bencher) {
     let tune = Tunables::default();
     let sink = BodySink::with_fs(test_path().unwrap()).unwrap();
     let body = sink_data(sink).unwrap();
-    let mut rt = tokio::runtime::Runtime::new().unwrap();
+    let mut rt = ThRuntime::new().unwrap();
     b.iter(|| {
         let stream = AsyncBodyImage::new(body.clone(), &tune);
         summarize_stream(stream, &mut rt);
@@ -91,13 +94,13 @@ fn stream_10_fsread(b: &mut Bencher) {
     rt.shutdown_on_idle();
 }
 
-// `UniBodyImage` in `FsRead`, default buffer size (64K)
+// `UniBodyImage` in `FsRead`, default buffer size (64K), threaded runtime.
 #[bench]
-fn stream_11_fsread_uni(b: &mut Bencher) {
+fn stream_20_fsread_uni(b: &mut Bencher) {
     let tune = Tunables::default();
     let sink = BodySink::with_fs(test_path().unwrap()).unwrap();
     let body = sink_data(sink).unwrap();
-    let mut rt = tokio::runtime::Runtime::new().unwrap();
+    let mut rt = ThRuntime::new().unwrap();
     b.iter(|| {
         let stream = UniBodyImage::new(body.clone(), &tune);
         summarize_stream(stream, &mut rt);
@@ -105,13 +108,104 @@ fn stream_11_fsread_uni(b: &mut Bencher) {
     rt.shutdown_on_idle();
 }
 
+// `UniBodyImage` in `FsRead`, default buffer size (64K), threaded runtime,
+// dispatch pool.
+#[bench]
+fn stream_21_fsread_uni_dispatch(b: &mut Bencher) {
+    lazy_static! {
+        static ref POOL: DispatchPool =
+            DispatchPool::builder().pool_size(2).create();
+    }
+    let tune = Tunables::default();
+    let sink = BodySink::with_fs(test_path().unwrap()).unwrap();
+    let body = sink_data(sink).unwrap();
+    let mut rt = ThBuilder::new()
+        .core_threads(2)
+        .blocking_threads(1000)
+        .after_start(move || {
+            DispatchPool::register_thread_local(POOL.clone());
+        })
+        .build()
+        .expect("ThRuntime build");
+    b.iter(|| {
+        let stream = UniBodyImage::new(body.clone(), &tune);
+        summarize_stream(stream, &mut rt);
+    });
+    rt.shutdown_on_idle();
+}
+
+// `UniBodyImage` in `FsRead`, default buffer size (64K), threaded runtime,
+// dispatch queue length 0.
+#[bench]
+fn stream_22_fsread_uni_dispatch_ql0(b: &mut Bencher) {
+    lazy_static! {
+        static ref POOL: DispatchPool =
+            DispatchPool::builder().pool_size(2).queue_length(0).create();
+    }
+    let tune = Tunables::default();
+    let sink = BodySink::with_fs(test_path().unwrap()).unwrap();
+    let body = sink_data(sink).unwrap();
+    let mut rt = ThBuilder::new()
+        .core_threads(2)
+        .blocking_threads(1000)
+        .after_start(move || {
+            DispatchPool::register_thread_local(POOL.clone());
+        })
+        .build()
+        .expect("ThRuntime build");
+    b.iter(|| {
+        let stream = UniBodyImage::new(body.clone(), &tune);
+        summarize_stream(stream, &mut rt);
+    });
+    rt.shutdown_on_idle();
+}
+
+// `UniBodyImage` in `FsRead`, default buffer size (64K), current thread
+// runtime, dispatch pool
+#[bench]
+fn stream_23_fsread_uni_dispatch_ct(b: &mut Bencher) {
+    let pool = DispatchPool::builder()
+        .pool_size(2)
+        .create();
+    DispatchPool::register_thread_local(pool);
+
+    let tune = Tunables::default();
+    let sink = BodySink::with_fs(test_path().unwrap()).unwrap();
+    let body = sink_data(sink).unwrap();
+    let mut rt = CtRuntime::new().unwrap();
+    b.iter(|| {
+        let stream = UniBodyImage::new(body.clone(), &tune);
+        summarize_stream(stream, &mut rt);
+    });
+}
+
+// `UniBodyImage` in `FsRead`, default buffer size (64K), current thread
+// runtime, dispatch queue length 0.
+#[bench]
+fn stream_24_fsread_uni_dispatch_ct_ql0(b: &mut Bencher) {
+    let pool = DispatchPool::builder()
+        .pool_size(2)
+        .queue_length(0)
+        .create();
+    DispatchPool::register_thread_local(pool);
+
+    let tune = Tunables::default();
+    let sink = BodySink::with_fs(test_path().unwrap()).unwrap();
+    let body = sink_data(sink).unwrap();
+    let mut rt = CtRuntime::new().unwrap();
+    b.iter(|| {
+        let stream = UniBodyImage::new(body.clone(), &tune);
+        summarize_stream(stream, &mut rt);
+    });
+}
+
 // `AsyncBodyImage` in `FsRead`, 8KiB buffer size
 #[bench]
-fn stream_12_fsread_8k(b: &mut Bencher) {
+fn stream_30_fsread_8k(b: &mut Bencher) {
     let tune = Tuner::new().set_buffer_size_fs(8 * 1024).finish();
     let sink = BodySink::with_fs(test_path().unwrap()).unwrap();
     let body = sink_data(sink).unwrap();
-    let mut rt = tokio::runtime::Runtime::new().unwrap();
+    let mut rt = ThRuntime::new().unwrap();
     b.iter(|| {
         let stream = AsyncBodyImage::new(body.clone(), &tune);
         summarize_stream(stream, &mut rt);
@@ -121,11 +215,11 @@ fn stream_12_fsread_8k(b: &mut Bencher) {
 
 // `AsyncBodyImage` in `FsRead`, 128KiB buffer size
 #[bench]
-fn stream_13_fsread_128k(b: &mut Bencher) {
+fn stream_31_fsread_128k(b: &mut Bencher) {
     let tune = Tuner::new().set_buffer_size_fs(128 * 1024).finish();
     let sink = BodySink::with_fs(test_path().unwrap()).unwrap();
     let body = sink_data(sink).unwrap();
-    let mut rt = tokio::runtime::Runtime::new().unwrap();
+    let mut rt = ThRuntime::new().unwrap();
     b.iter(|| {
         let stream = AsyncBodyImage::new(body.clone(), &tune);
         summarize_stream(stream, &mut rt);
@@ -135,11 +229,11 @@ fn stream_13_fsread_128k(b: &mut Bencher) {
 
 // `AsyncBodyImage` in `FsRead`, 1MiB buffer size
 #[bench]
-fn stream_14_fsread_1m(b: &mut Bencher) {
+fn stream_32_fsread_1m(b: &mut Bencher) {
     let tune = Tuner::new().set_buffer_size_fs(1024 * 1024).finish();
     let sink = BodySink::with_fs(test_path().unwrap()).unwrap();
     let body = sink_data(sink).unwrap();
-    let mut rt = tokio::runtime::Runtime::new().unwrap();
+    let mut rt = ThRuntime::new().unwrap();
     b.iter(|| {
         let stream = AsyncBodyImage::new(body.clone(), &tune);
         summarize_stream(stream, &mut rt);
@@ -149,11 +243,11 @@ fn stream_14_fsread_1m(b: &mut Bencher) {
 
 // `AsyncBodyImage` in `FsRead`, 4MiB buffer size
 #[bench]
-fn stream_15_fsread_4m(b: &mut Bencher) {
+fn stream_33_fsread_4m(b: &mut Bencher) {
     let tune = Tuner::new().set_buffer_size_fs(4 * 1024 * 1024).finish();
     let sink = BodySink::with_fs(test_path().unwrap()).unwrap();
     let body = sink_data(sink).unwrap();
-    let mut rt = tokio::runtime::Runtime::new().unwrap();
+    let mut rt = ThRuntime::new().unwrap();
     b.iter(|| {
         let stream = AsyncBodyImage::new(body.clone(), &tune);
         summarize_stream(stream, &mut rt);
@@ -163,11 +257,11 @@ fn stream_15_fsread_4m(b: &mut Bencher) {
 
 #[bench]
 // `UniBodyImage` in `MemMap`, mmap once ahead-of-time, zero-copy
-fn stream_20_mmap_uni_pre(b: &mut Bencher) {
+fn stream_40_mmap_uni_pre(b: &mut Bencher) {
     let tune = Tunables::default();
     let sink = BodySink::with_fs(test_path().unwrap()).unwrap();
     let mut body = sink_data(sink).unwrap();
-    let mut rt = tokio::runtime::Runtime::new().unwrap();
+    let mut rt = ThRuntime::new().unwrap();
     body.mem_map().unwrap();
     b.iter(|| {
         let stream = UniBodyImage::new(body.clone(), &tune);
@@ -178,11 +272,11 @@ fn stream_20_mmap_uni_pre(b: &mut Bencher) {
 
 // `UniBodyImage` in `MemMap`, new mmap on each iteration, zero-copy
 #[bench]
-fn stream_21_mmap_uni(b: &mut Bencher) {
+fn stream_41_mmap_uni(b: &mut Bencher) {
     let tune = Tunables::default();
     let sink = BodySink::with_fs(test_path().unwrap()).unwrap();
     let body = sink_data(sink).unwrap();
-    let mut rt = tokio::runtime::Runtime::new().unwrap();
+    let mut rt = ThRuntime::new().unwrap();
     b.iter(|| {
         let mut body = body.clone();
         body.mem_map().unwrap();
@@ -195,11 +289,11 @@ fn stream_21_mmap_uni(b: &mut Bencher) {
 // `AsyncBodyImage` in `MemMap`, new mmap on each iteration, and with costly
 // copy to `Bytes`.
 #[bench]
-fn stream_22_mmap_copy(b: &mut Bencher) {
+fn stream_42_mmap_copy(b: &mut Bencher) {
     let tune = Tunables::default();
     let sink = BodySink::with_fs(test_path().unwrap()).unwrap();
     let body = sink_data(sink).unwrap();
-    let mut rt = tokio::runtime::Runtime::new().unwrap();
+    let mut rt = ThRuntime::new().unwrap();
     b.iter(|| {
         let mut body = body.clone();
         body.mem_map().unwrap();
@@ -209,10 +303,11 @@ fn stream_22_mmap_copy(b: &mut Bencher) {
     rt.shutdown_on_idle();
 }
 
-fn summarize_stream<S, T, E>(stream: S, rt: &tokio::runtime::Runtime)
+fn summarize_stream<S, T, E, R>(stream: S, rt: &mut R)
     where S: Stream<Item = Result<T, E>> + StreamExt + Send + 'static,
           T: AsRef<[u8]>,
-          E: std::fmt::Debug
+          E: std::fmt::Debug,
+          R: RuntimeExt
 {
     let task = stream.fold((0u8, 0), |(mut ml, len), item| {
         let item = item.unwrap();
@@ -230,6 +325,9 @@ fn summarize_stream<S, T, E>(stream: S, rt: &tokio::runtime::Runtime)
     assert_eq!(len, 8192 * 1024);
 }
 
+/// Return a new body prepared for read, after writing 8MiB of data the the
+/// given sink (of any state). All possible u8 values are randomly
+/// located within this body.
 fn sink_data(mut body: BodySink) -> Result<BodyImage, BodyError> {
     let reps = 1024;
     let mut vals: Vec<u8> = (0..reps).map(|v| (v % 256) as u8).collect();
