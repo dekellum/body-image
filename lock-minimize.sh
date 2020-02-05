def037a87949b3a54838ec1fea2ed4f65d51f25d
David Kellum <dek-oss@gravitext.com>
2020-02-05 12:55:15 -0800

    try another min-ver workaround for old winapi

--- lock-minimize.sh
+++ lock-minimize.sh
@@ -2,27 +2,14 @@
 
 cargo -Z minimal-versions generate-lockfile
 
-# failing on windows currently:
-# net2 v0.2.32
+# net2 v0.2.32 or 33 fails to build on windows, with older winapi:
 # ├── hyper v0.13.1
-# │   ├── body-image-futio v2.1.0 (/home/david/src/body-image/body-image-futio)
-# │   └── hyper-tls v0.4.0
-# │       └── body-image-futio v2.1.0 (/home/david/src/body-image/body-image-futio) (*)
 # ├── mio v0.6.20
-# │   └── tokio v0.2.6
-# │       ├── blocking-permit v1.2.0
-# │       │   └── body-image-futio v2.1.0 (/home/david/src/body-image/body-image-futio) (*)
-# │       ├── body-image-futio v2.1.0 (/home/david/src/body-image/body-image-futio) (*)
-# │       ├── h2 v0.2.1
-# │       │   └── hyper v0.13.1 (*)
-# │       ├── hyper v0.13.1 (*)
-# │       ├── hyper-tls v0.4.0 (*)
-# │       ├── tokio-tls v0.3.0
-# │       │   └── hyper-tls v0.4.0 (*)
-# │       └── tokio-util v0.2.0
-# │           └── h2 v0.2.1 (*)
-# │       [dev-dependencies]
-# │       └── body-image-futio v2.1.0 (/home/david/src/body-image/body-image-futio) (*)
 # └── miow v0.2.1
 #     └── mio v0.6.20 (*)
-cargo update -p net2 --precise 0.2.33
+# error[E0432]: unresolved import `winapi::shared::ws2ipdef::SOCKADDR_IN6_LH`
+#   --> C:\Users\appveyor\.cargo\registry\src\github.com-1ecc6299db9ec823\net2-0.2.33\src\sys\windows\mod.rs:38:13
+#    |
+# 38 |     pub use winapi::shared::ws2ipdef::SOCKADDR_IN6_LH as sockaddr_in6;
+#    |             ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ no `SOCKADADR_IN6_LH` in `shared::ws2ipdef
+cargo update -p winapi:0.3.0 --precise=0.3.6
